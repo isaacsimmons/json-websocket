101319922b7c978711aa268db751c4f89e5911c4
Isaac Simmons <isaac.simmons@gmail.com>
2013-03-21 22:32:15 -0400

    Tweaks to test-server script to capture update messages

--- test/test-server.js
+++ test/test-server.js
@@ -3,20 +3,30 @@
 var http = require('http').createServer();
 var Server = require('../lib/main.js').server;
 
+var SERVER_VAL = 123;
+
 var server = Server({ httpServer: http, verbose: true });
 
-var clientValue = null;
+var clients = {};
 
 server.on('hello', function(clientId, greeting, value) {
-  clientValue = value;
+  clients[clientId].push(value);
 });
 
 server.on('update', function(clientId, value) {
-  clientValue = value;
+  clients[clientId].push(value);
 });
 
-server.on('js:connect', function(id) {
-  server.send(id, 'hello', 'Greetings, client #' + id, 123);
+server.on('js:connect', function(clientId) {
+  clients[clientId] = [];
+  server.send(clientId, 'hello', 'Greetings, client #' + clientId, SERVER_VAL);
+});
+
+server.on('js:disconnect', function(clientId) {
+  console.log('Client #' + clientId + ' value summary');
+  console.log(JSON.stringify(clients[clientId]));
+  //TODO: assert zero, SERVER_VAL, 100
+  delete clients[clientId];
 });
 
 http.listen(8000);
